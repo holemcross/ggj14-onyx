b9a3fc24e36c1bd66c8ae7b273eb224991551228
bongikairu <bongikairu@gmail.com>
2014-01-25 13:56:46 -0600

    Add central game resource object

---
+++ onyx/Assets/Logic/GameResources.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameResources : MonoBehaviour {
+
+    public int p1_mana;
+    public int p2_mana;
+
+    public int pop_all;
+    public int pop_p1;
+    public int pop_p2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
