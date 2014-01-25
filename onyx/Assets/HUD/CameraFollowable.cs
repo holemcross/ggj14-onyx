7c8a64cfad22b556a0960c8471028bf192f74409
bongikairu <bongikairu@gmail.com>
2014-01-25 13:28:54 -0600

    Camera, CameraFollowable, Basic HUD for camera control

---
+++ onyx/Assets/HUD/CameraFollowable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowable : MonoBehaviour {
+
+    public float getCurrentX()
+    {
+        return transform.position.x;
+    }
+
+}
+
+
---
+++ onyx/Assets/HUD/PlayerCameraPanner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCameraPanner : MonoBehaviour {
+
+    public PlayerCamera camera;
+    public bool rightPan = true;
+
+    private bool isHovering = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isHovering)
+        {
+            if (rightPan) camera.PanRight();
+            else camera.PanLeft();
+        }
+	}
+
+    void OnHover(bool isOver)
+    {
+        isHovering = isOver;
+    }
+
+}
